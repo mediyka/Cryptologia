1d4ee5ec680fe9529d1b5552ca66477ae58dd338
mediyka <158853505+mediyka@users.noreply.github.com>
2026-02-26 14:37:39 +0300

    Update main.py

--- cryptosafe-manager/main.py
+++ cryptosafe-manager/main.py
@@ -1,1 +1,15 @@
+#!/usr/bin/env python3
+"""
+Точка входа в приложение CryptoSafe Manager.
+"""
+from src.gui.main_window import MainWindow
 
+
+def main():
+    """Запускает приложение."""
+    app = MainWindow()
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
